2787c38f6f20285abe7600f18302101d2fc8229d
John Li <johnli0135@gmail.com>
2018-09-30 14:13:08 -0400

    add/test Workspace.area

--- workspace.py
+++ workspace.py
@@ -1,6 +1,25 @@
 from util import *
+from window import *
 from typing import *
 
+# multiple windows on the same screen
+class Workspace:
+  def __init__(self):
+    self.windows = {} # map from window ids to Window instances
+
+  # estimate the percentage of total pixels on the screen occupied by windows
+  def area(self, samples=10000) -> float:
+    from math import sqrt
+    delta = sqrt(screen_pixels / samples)
+
+    hits = 0
+    for i in range(int(screen_width / delta)):
+      for j in range(int(screen_height / delta)):
+        left = int(i * delta)
+        top = int(j * delta)
+        hits += int(any(w.contains(left, top) for _, w in self.windows.items()))
+
+    return hits / samples
 
   # apply the window properties to the actual window
   def render(self):
@@ -8,7 +27,7 @@ from typing import *
 
     # set maximize/unmaximize properly
     s = 'wmctrl -i -r {} -b {},maximized_horz,maximized_vert'
-    system(s.format(self.id, "add" if self.maximized else "remove"))
+    system(s.format(self.id, 'add' if self.maximized else 'remove'))
 
     # if positions/sizes were fiddled with enough to be noticeable, adjust them irl
     if self.modified:
@@ -17,3 +36,23 @@ from typing import *
       system(s)
 
     return self
+
+if __name__ == '__main__':
+  from time import time
+
+  a = Workspace()
+  a.windows[0] = Window(10, 10, 100, 100)
+
+  n = 1000
+  result = None
+  start = time()
+  for _ in range(n):
+    result = a.area()
+  elapsed = time() - start
+
+  print('{} ms elapsed for {} samples ({} ms per), yielding {}'.format(
+    round(elapsed, 3),
+    n,
+    round(elapsed / n, 3),
+    result))
+  
